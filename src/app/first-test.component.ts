39f6fa4ff75e46c64b8a76d2739999254524330a
Adolfo <adolfoo.bcn@gmail.com>
2022-05-17 23:02:58 +0200

    primer commit

--- src/app/first-test.component.ts
+++ src/app/first-test.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'first-test',
-    template: '<p>Primera prueba</p>'
+    template: '<p>Primera prueba   </p>'
 })
 
 export class FirstComponent{}
