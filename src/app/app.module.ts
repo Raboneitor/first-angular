9d5b49be83f250f99697fb62fb841c2accf7fc37
Adolfo <adolfoo.bcn@gmail.com>
2022-05-22 22:54:01 +0200

    Nav and coverimage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first-test.component';
+import { CoverComponent } from './cover/cover.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent, FirstComponent
+    AppComponent, FirstComponent, NavComponent, CoverComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cover/cover.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cover',
+    templateUrl: './cover.component.html',
+    styleUrls: ['./cover.component.css']
+})
+
+export class CoverComponent {
+
+}
